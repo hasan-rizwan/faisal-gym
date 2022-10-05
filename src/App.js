048e218089d0b15767bec31bbc164a319a4aee66
hasan <hasanrizwan6@gmail.com>
2022-10-06 04:13:20 +0500

    built the whole Program component, but the colors need to change

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
-// import Program from './components/Program';
+import Program from './components/Program';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Home />
-      {/* <Program /> */}
+      <Program />
     </div>
   );
 }
--- src/components/Program.js
+++ src/components/Program.js
@@ -1,17 +1,67 @@
 import React from 'react'
 import './styles/program.css'
+import program1 from './styles/assets/images/program1.png'
+import program2 from './styles/assets/images/program2.png'
+import program3 from './styles/assets/images/program3.png'
+import rightArrow from './styles/assets/webfonts/arrow-right-line.svg'
 
 const Program = () => {
     return (
-        <div className='program'>
-            <h1>Our Program</h1>
-            <h2>Start your Journey</h2>
-            <div className="card-container">
-                <div className="card">faad</div>
-                <div className="card">dsasad</div>
-                <div className="card">adasdcxv</div>
+        <section className='program section'>
+            <div className="container">
+                <div className="section-data">
+                    <h2 className="section-subtitle">Our Program</h2>
+                    <div className="section-titles">
+                        <h1 className="section-title-border">BUILD YOUR</h1>
+                        <h1 className="section-title">DREAM BODY</h1>
+                    </div>
+                </div>
+                <div className="program-container grid">
+                    <article className="program-card">
+                        <div className="program-shape">
+                            <img src={program1} alt="program icon" className='program-img' />
+                        </div>
+                        <h3 className="program-title">Flex Muscle</h3>
+                        <p className="program-description">Creating tension that's temporarily making the muscle fibers smaller or contracted.</p>
+                        <a href="/" className='program-button'>
+                            <img src={rightArrow} alt="Right Arrow" />
+                        </a>
+                    </article>
+
+                    <article className="program-card">
+                        <div className="program-shape">
+                            <img src={program2} alt="program icon" className='program-img' />
+                        </div>
+                        <h3 className="program-title">Cardio Exercise</h3>
+                        <p className="program-description">Exercise your heart rate up and keeps it up for a prolonged period of time.</p>
+                        <a href="/" className='program-button'>
+                            <img src={rightArrow} alt="Right Arrow" />
+                        </a>
+                    </article>
+
+                    <article className="program-card">
+                        <div className="program-shape">
+                            <img src={program3} alt="program icon" className='program-img' />
+                        </div>
+                        <h3 className="program-title">Basic Yoga</h3>
+                        <p className="program-description">Diaphragmatic this is the most common breathing technique you'll find in yoga.</p>
+                        <a href="/" className='program-button'>
+                            <img src={rightArrow} alt="Right Arrow" />
+                        </a>
+                    </article>
+                    <article className="program-card">
+                        <div className="program-shape">
+                            <img src={program3} alt="program icon" className='program-img' />
+                        </div>
+                        <h3 className="program-title">Weight Lifting</h3>
+                        <p className="program-description">Attempts a maximum weight single lift of a barbell loaded with weight plates.</p>
+                        <a href="/" className='program-button'>
+                            <img src={rightArrow} alt="Right Arrow" />
+                        </a>
+                    </article>
+                </div>
             </div>
-        </div>
+        </section>
     )
 }
 
